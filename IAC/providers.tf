8d61b7841dc18295b5402c78248728b32ef9b930
Lingaiah Banda <57674648+LingaiahB@users.noreply.github.com>
2025-07-19 17:48:47 +0530

    Create providers.tf

---
+++ IAC/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+   subscription_id = "90de0c98-2d25-4094-a834-32b29fdf8003"
+   tenant_id = "0b889ca3-c111-47fc-b0ed-88a37593b8fa"
+}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "HealthCareRG"
+    storage_account_name = "healthcaresalingaiah"
+    container_name       = "tfstatecontainer"
+    key                  = "aks.tfstate"
+  }
+}
